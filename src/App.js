cb1c226346fbeb7b76a93952a9ab76dde278fb49
mxtmohit <81439541+mxtmohit@users.noreply.github.com>
2023-11-08 20:25:29 +0530

    prod build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import { Skeleton } from '@mui/material';
 import './App.css';
-import DisplayInfo from './homepage/appcomponents/displayInfo/DisplayInfo';
-import Codeinput from './homepage/appcomponents/enterPostalcode/Codeinput';
+import DisplayInfo from './comps/appcomponents/displayInfo/DisplayInfo';
+import Codeinput from './comps/appcomponents/enterPostalcode/Codeinput';
 
 function App() {
   return (
